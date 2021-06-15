7e174974774dfc9c6cc0ed5fe9ef41fa44d4b1b9
da-the-dev <leshiktk@me.com>
2021-06-15 20:19:34 +0300

    Added an .av command

---
+++ commands/av.ts
@@ -0,0 +1,28 @@
+import { MessageEmbed } from 'discord.js'
+import { BaseCommand } from '../intefaces/BaseCommand'
+import embed from '../utility/embed'
+
+const sMsg = 'Просмотр аватара'
+/**
+ * @example Usage: `.av @member`
+ */
+const command: BaseCommand = {
+    foo: (msg, args, client) => {
+        const mMember = msg.mentions.members.first() || msg.member
+        const emb = embed(msg, sMsg, `<@${mMember.id}>`, false, false) as MessageEmbed
+        emb
+            .setThumbnail('')
+            .setImage(mMember.user.displayAvatarURL({ dynamic: true }) + "?size=2048")
+
+        msg.channel.send(emb)
+    },
+    help: (msg) => {
+        embed(msg, sMsg + ': помощь', `
+        Пример работы команды:
+        \`.av - выводит Ваш аватар\`
+        \`.av @Kanto - выводит Ваш аватар пользователя Kanto\`
+        `) as MessageEmbed
+    },
+    description: 'Показывает аватар участника'
+}
+export = command
