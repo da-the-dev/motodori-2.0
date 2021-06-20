4bf0df9e9f568e1fad94893e26ffc7135617a1ba
da-the-dev <leshiktk@me.com>
2021-06-20 15:59:56 +0300

    Added .bal

---
+++ commands/bal.ts
@@ -0,0 +1,21 @@
+import { BaseCommand } from '../interfaces/BaseCommand'
+import { DBUser } from '../classes/DB'
+import embed from '../utility/embed'
+
+const sMsg = 'Баланс'
+/** @example Usage: `.bal @member?` */
+const command: BaseCommand = {
+    foo: async (msg, args, client) => {
+        const mMember = msg.mentions.members.first() || msg.member
+        embed(msg, sMsg, `У <@${mMember.id}> на балансе **${(await new DBUser(mMember.guild.id, mMember.id).fetch()).data.money}**`)
+    },
+    help: (msg) => {
+        embed(msg, sMsg + ': помощь', `
+        Пример работы команды:
+        \`.bal - выводит Ваш баланс\`
+        \`.bal @Kanto - выводит Ваш баланс пользователя Kanto\`
+        `)
+    },
+    description: 'Показывает баланс участника'
+}
+export = command
