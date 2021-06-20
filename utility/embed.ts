b7bbc0f4cb49a56e49f6c1a1e320d0c2d10e17e6
da-the-dev <leshiktk@me.com>
2021-06-20 16:09:56 +0300

    Fixed pinging to only make the first letter lowercase

--- utility/embed.ts
+++ utility/embed.ts
@@ -4,7 +4,7 @@ function embed(msg: Message, title: string, content = '', ping = false, send = t
     const emb = new MessageEmbed()
         .setTitle(`${title}`)
         .setColor('#2F3136')
-        .setDescription(`${ping ? `${msg.author}, ` : ''} ${ping ? content.toLowerCase() || '' : content || ''}`)
+        .setDescription(`${ping ? `${msg.author}, ` : ''} ${ping ? content[0].toLowerCase() + content.slice(1) || '' : content || ''}`)
         .setFooter(msg.member.displayName, msg.author.displayAvatarURL({ dynamic: true }))
     return send ? msg.channel.send(emb) : emb
 }
