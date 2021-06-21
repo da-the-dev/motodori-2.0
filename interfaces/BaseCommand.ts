f7442676eb4fd0f2d5d86f0aa31296abb8ce34a7
da-the-dev <leshiktk@me.com>
2021-06-21 23:54:26 +0300

    Added flag field

--- interfaces/BaseCommand.ts
+++ interfaces/BaseCommand.ts
@@ -2,5 +2,6 @@ import { Message, Client } from "discord.js"
 export interface BaseCommand {
     description: string,
     foo: (msg: Message, args: string[], client: Client) => void,
-    help: (msg) => void
+    help: (msg) => void,
+    flag: string
 }
