5bbf90deeca543f1f519004c12ce997eae22aaac
da-the-dev <leshiktk@me.com>
2021-06-22 17:46:49 +0300

    Added button hierarchy

--- classes/Menu.ts
+++ classes/Menu.ts
@@ -1,5 +1,6 @@
 import { MessageButton, ExtendedMessage, ExtendedMessageOptions, MessageComponent, ButtonCollector, ExtendedWebhookClient } from 'discord-buttons'
 import { MessageEmbed, TextChannel, DMChannel, NewsChannel, User } from 'discord.js';
+import { logger } from '../utility/logger';
 import Button from './Button';
 import Page from './Page';
 
@@ -14,9 +15,9 @@ export class Menu {
     private pages: Page[] = []
     private currentMessage: ExtendedMessage
     private clicker: User
-    private channel: TextChannel | NewsChannel | DMChannel
+    private channel: TextChannel
 
-    constructor(firstPage: Page, clicker: User, channel: TextChannel | NewsChannel | DMChannel) {
+    constructor(firstPage: Page, clicker: User, channel: TextChannel) {
         if (firstPage.name != 'main')
             throw { message: 'Incorrect name of the first page', name: 'FIRST_PAGE_NOT_MAIN' }
 
@@ -26,17 +27,22 @@ export class Menu {
     }
 
     setPage(page: Page) {
-        if (page.prev)
+        if (page.prev) {
             page.buttons.push(new Button()
                 .setButton(new MessageButton()
                     .setStyle(2)
                     .setLabel('Назад')
-                    .setID(`${page.name}-back`))
+                    .setID(`back`))
                 .setAction(async menu => {
                     await menu.sendPage(page.prev.name)
                 })
             )
-        else
+
+            page.buttons.forEach(b => {
+                b.button.custom_id = `${page.prev.name}-${page.name}-${b.button.custom_id}`
+            })
+        }
+        else {
             page.buttons.push(new Button()
                 .setButton(new MessageButton()
                     .setStyle(2)
@@ -47,6 +53,15 @@ export class Menu {
                 })
             )
 
+            page.buttons.forEach(b => {
+                b.button.custom_id = `main-${page.name}-${b.button.custom_id}`
+            })
+        }
+
+        page.buttons.forEach(b => {
+            b.button.custom_id = `${this.channel.guild.id}-${b.button.custom_id}`
+        })
+
         this.pages.push(page)
         return this
     }
@@ -55,6 +70,9 @@ export class Menu {
         const page = this.pages.find(p => p.name == 'main')
         this.currentMessage = await this.channel.send({ embed: page.embed, buttons: page.buttons.map(b => b.button) } as ExtendedMessageOptions) as ExtendedMessage
         this.addListener(page)
+
+        logger.debug(this.pages.map(p => p.buttons.map(b => b.button.custom_id)))
+
         return this
     }
 
