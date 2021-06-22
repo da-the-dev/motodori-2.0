7efafae7a2fc386db50c884ad164cc1268d59f80
da-the-dev <leshiktk@me.com>
2021-06-22 23:37:00 +0300

    Added everything needed about roles setup and demo setup function

--- commands/serverSetup/settings.ts
+++ commands/serverSetup/settings.ts
@@ -9,6 +9,7 @@ const defaultButton = {
 }
 
 // *Pages*
+// Main page
 const settingsMenu: Page = {
     name: 'settings',
     embed: new MessageEmbed(
@@ -29,11 +30,12 @@ const settingsMenu: Page = {
     ]
 }
 
+// Roles setup
 const rolesSetup: Page = {
     name: 'rolesSetup',
     embed: new MessageEmbed({
         "title": "Настройка ролей",
-        "description": "Для перехода к настройкам ролей, выберете одну из ролей ниже",
+        "description": "Для перехода к настройкам ролей, выберете одну из них ниже\n**Описание ролей:**\n`1.` *Администрация* ⏤ администраторы сервера\n`2.` *Модераторы* ⏤ модераторы сервера, на уровень ниже администрации\n`3.` *Чат модераторы* ⏤ модераторы сервера, ответственные за порядок в текстовых чатах. На уровень ниже модераторов.\n`4.` *Войс модераторы* ⏤ модераторы сервера, ответственные за порядок в голосовых чатах. На уровень ниже чат модераторов.",
         "color": 3092790
     }),
     buttons: [
@@ -47,30 +49,31 @@ const rolesSetup: Page = {
             }),
         new Button()
             .setButton(new MessageButton(defaultButton)
-                .setLabel('Чат модерация')
+                .setLabel('Модератор')
+                .setID('moderator')
+            )
+            .setAction(async (menu, button) => {
+                await menu.sendPage('moderatorSetup')
+            }),
+        new Button()
+            .setButton(new MessageButton(defaultButton)
+                .setLabel('Чат модераторы')
                 .setID('chatMod')
             )
             .setAction(async (menu, button) => {
-                const m = await menu.sendEmbed(new MessageEmbed({
-                    "title": "Настройки сервера: настройка ролей: чат стафф",
-                    "description": "Теперь упомяните роль и отправьте сообщение",
-                    "color": 3092790
-                }))
-                const filter = (messsage: Message) => messsage.author.id === button.clicker.user.id
-                try {
-                    const role = (await m.channel.awaitMessages(filter, { time: 10000, max: 1 })).first().mentions.roles.first()
-                    button.message.channel.send(role.id)
-                } catch (error) {
-                    if (error instanceof TypeError)
-                        await menu.delete()
-                    else
-                        throw error
-                }
+                await menu.sendPage('chatModSetup')
+            }),
+        new Button()
+            .setButton(new MessageButton(defaultButton)
+                .setLabel('Голосовая модерация')
+                .setID('voiceMod')
+            )
+            .setAction(async (menu, button) => {
+                await menu.sendPage('voiceModSetup')
             })
     ],
     prev: settingsMenu
 }
-
 const adminSetup: Page = {
     name: 'adminSetup',
     embed: new MessageEmbed({
@@ -93,7 +96,28 @@ const adminSetup: Page = {
     },
     prev: rolesSetup
 }
-
+const moderatorSetup: Page = {
+    name: 'moderatorSetup',
+    embed: new MessageEmbed({
+        "title": "Модераторы",
+        "description": "Теперь упомяните роль и отправьте сообщение",
+        "color": 3092790
+    }),
+    buttons: [],
+    action: async menu => {
+        try {
+            const filter = (messsage: Message) => messsage.author.id === menu.clicker.id
+            const role = (await menu.channel.awaitMessages(filter, { time: 10000, max: 1 })).first().mentions.roles.first()
+            menu.channel.send(role.id)
+        } catch (error) {
+            if (error instanceof TypeError)
+                await menu.delete()
+            else
+                throw error
+        }
+    },
+    prev: rolesSetup
+}
 const chatModSetup: Page = {
     name: 'chatModSetup',
     embed: new MessageEmbed({
@@ -116,6 +140,29 @@ const chatModSetup: Page = {
     },
     prev: rolesSetup
 }
+const voiceModSetup: Page = {
+    name: 'voiceModSetup',
+    embed: new MessageEmbed({
+        "title": "Войс модерация",
+        "description": "Теперь упомяните роль и отправьте сообщение",
+        "color": 3092790
+    }),
+    buttons: [],
+    action: async menu => {
+        try {
+            const filter = (messsage: Message) => messsage.author.id === menu.clicker.id
+            const role = (await menu.channel.awaitMessages(filter, { time: 10000, max: 1 })).first().mentions.roles.first()
+            menu.channel.send(role.id)
+        } catch (error) {
+            if (error instanceof TypeError)
+                await menu.delete()
+            else
+                throw error
+        }
+    },
+    prev: rolesSetup
+}
+
 
 
 const sMsg = 'Настройки сервера'
@@ -125,7 +172,9 @@ const command: BaseCommand = {
         await new Menu(settingsMenu, msg.author, msg.channel as TextChannel)
             .setPage(rolesSetup)
             .setPage(adminSetup)
+            .setPage(moderatorSetup)
             .setPage(chatModSetup)
+            .setPage(voiceModSetup)
             .send()
     },
     help: (msg) => {
