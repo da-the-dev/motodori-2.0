137a8a8f52ddfab3c0a01c93eb6983b9491b8cbf
da-the-dev <leshiktk@me.com>
2021-06-25 21:40:00 +0300

    Added new buttons

--- headers/classes.ts
+++ headers/classes.ts
@@ -4,5 +4,7 @@ export * from '../classes/DBUser'
 export * from '../classes/DBServer'
 export * from '../classes/Menu'
 import Button from '../classes/Button'; export { Button }
+import OneWay from '../classes/OneWay'; export { OneWay }
+import Toggle from '../classes/Toggle'; export { Toggle }
 import Menu from '../classes/Menu'; export { Menu }
 import Page from '../classes/Page'; export { Page }
